662b358643c4d4e6c7babaf4a8617bac5b189b94
ankurwasnik <ankurwasnik@icloud.com>
2023-08-15 22:46:32 +0530

    Initial Commit

---
+++ H4kr News/H4kr_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4kr_NewsApp.swift
+//  H4kr News
+//
+//  Created by Ankur Wasnik on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct H4kr_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
